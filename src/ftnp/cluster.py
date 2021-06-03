Fix calculation bugs

src/ftnp/cluster.py
@@ -5,13 +5,14 @@ from scipy.integrate import quad
 
 class Cluster:
     
-    def __init__(self, cluster_dim, tetta, cell_sectors_num) -> None:
+    def __init__(self, cluster_dim, tetta, cell_sectors_num, signal_to_noise_ratio) -> None:
         self.cluster_dim = cluster_dim
         self.tetta = tetta
         self.cell_sectors_num = cell_sectors_num # --> M
+        self.signal_to_noise_ratio = signal_to_noise_ratio 
 
     def get_attenuation_of_interfering_signals(self):
-        """Ослабление мешающих сигналов q """
+        """Ослабление мешающих сигналов q"""
         q = m.sqrt(3 * self.cluster_dim)
         return q
     
@@ -35,23 +36,27 @@ class Cluster:
             sum_one += betta_i[i] ** 2
             sum_two += betta_i[i]
         sum_two = sum_two ** 2
-        tetta_e_squared = (1/0.053) * m.log(1 + int(np.exp(0.053 * self.tetta **2 ) - 1 ) * sum_one / sum_two)
+        tetta_e_squared = (1/0.053) * m.log(
+            1 + (
+                np.exp(0.053 * (self.tetta ** 2),) - 1
+            ) * sum_one / sum_two
+        )
         return tetta_e_squared
 
     def get_betta(self):
         """относительный уровень суммарной помехи по основному канала приема"""
         betta_i = self.derive_betta_i()
         sum_b = 0
-        for i in range(1, betta_i[0]+1):
+        for i in range(1, betta_i[0] + 1):
             sum_b = betta_i[i]
-        tetta = self.get_main_reception_channel_deviation()
-        betta = sum_b * np.exp(0.053 * ((tetta**2 - tetta**2) / 2))
+        tetta_squared = self.get_main_reception_channel_deviation()
+        betta = sum_b * np.exp((0.053 * (self.tetta ** 2 - tetta_squared)) / 2)
         return betta
     
     def derive_x1(self):
         tetta_squared = self.get_main_reception_channel_deviation()
         b = self.get_betta()
-        x1 = (10 * m.log(1/b) - 9) / m.sqrt(self.tetta**2 + tetta_squared)
+        x1 = (10 * m.log10(1/b) - self.signal_to_noise_ratio) / m.sqrt(self.tetta ** 2 + tetta_squared)
         return x1
 
     def get_signal_to_noise_failure_probability(self):
@@ -59,7 +64,6 @@ class Cluster:
         def integrand(x):
             return np.exp((-x**2) / 2) 
     
-        pi = 3.14
         x1 = self.derive_x1()
         I = quad(integrand, x1, np.inf)[0]
-        return (1/m.sqrt(2*pi)*I) * 100
+        return (1 / m.sqrt(2 * np.pi) * I) * 100